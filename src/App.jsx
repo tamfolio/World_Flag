16f5f28c3e0e895ad109310d3fc9eb835ea4ba07
unknown <tamiloreolaniyi@gmail.com>
2022-01-03 17:20:08 +0100

    display all countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
 import React, { Component } from 'react'
+import './App.css'
+import FlagComponent from './Components/FlagComponent/FlagComponent';
+import Header from './Components/Header/Header';
+import SearchBar from './Components/Search&Filter/SearchBar';
 
  class App extends Component {
 
   constructor() {
     super();
     this.state ={
-      countryData: []
+      countryData: [],
+      SearchBar: ''
     }
   }
 
   componentDidMount() {
     fetch("https://restcountries.com/v2/all")
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => 
+        this.setState({
+          countryData: response
+        })
+      )
   }
   render() {
-    return (
+      return (
       <div>
-        
-      </div>
+        <Header/>
+        <SearchBar/>
+        <div className="flexed-country">
+        {
+          this.state.countryData.map(({index, name, capital, population, region, flag}) => (
+            
+            <FlagComponent key={index} name={name} capital={capital} population={population} region={region} flag={flag}/>
+            
+          ))
+        }
+        </div>
+        </div>
     )
   }
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+          <h1>Where in the World?</h1>
+          <h3>Dark Mode</h3>
+        </div>
+    )
+}
+
+export default Header
